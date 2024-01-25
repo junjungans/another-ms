eef97c874cbf281f9133bef61bd5df55384b7541
Junjungan Siregar <73218259+junjungans@users.noreply.github.com>
2024-01-25 12:18:13 +0700

    Create kafka.test.js

---
+++ kafka.test.js
@@ -0,0 +1,38 @@
+const { MessageProviderPact, providerWithMetadata } = require('@pact-foundation/pact');
+
+describe('sampleRepository verification', () => {
+  describe('kafka producer verifier', () => {
+    describe('asyncCreate', () => {
+      const kafkaProducers = {
+        create: () => {
+          return new Promise((resolve) => {
+            resolve({
+              id: 'id'
+            });
+          });
+        }
+      };
+
+      const kafkaProvider = new MessageProviderPact({
+        messageProviders: {
+          'a message from kafka': providerWithMetadata(() => kafkaProducers.create(), {
+            queue: 'sample'
+          })
+        },
+        provider: 'kafka-another-ms',
+        providerVersion: process.env.GIT_COMMIT,
+        providerVersionBranch: process.env.GIT_BRANCH,
+        pactBrokerUrl: process.env.PROVIDER_URL,
+        pactBrokerUsername: 'userTesting',
+        pactBrokerPassword: 'userTestingPassword',
+        publishVerificationResult: true,
+        consumerVersionSelectors: [{ latest: true }]
+      });
+      describe('kafkaProducers', () => {
+        it('should verify consumer contract testing', () => {
+          return kafkaProvider.verify();
+        });
+      });
+    });
+  });
+});
